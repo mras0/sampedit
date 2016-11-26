165106d695684e5de920e92a3668d474c70f34b2
Michael Rasmussen <michaelrasmussen1337@gmail.com>
2016-11-26 11:55:19 +0100

    Move mixer to its own files

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 #include <base/job_queue.h>
 #include <base/sample_voice.h>
 #include "module.h"
-#include "win32/wavedev.h"
+#include "mixer.h"
 
 std::vector<float> create_sample(int len, float freq, int rate=44100)
 {
@@ -24,85 +24,6 @@ std::vector<float> create_sample(int len, float freq, int rate=44100)
     }
     return data;
 }
-
-class mixer {
-public:
-    explicit mixer()
-        : wavedev_(sample_rate_, 4096, [this](short* s, size_t num_stereo_samples) { render(s, static_cast<int>(num_stereo_samples)); }) {
-    }
-
-    int sample_rate() const {
-        return sample_rate_;
-    }
-
-    void at_next_tick(job_queue::job_type job) {
-        at_next_tick_.post(job);
-    }
-
-    void add_voice(voice& v) {
-        at_next_tick_.assert_in_queue_thread();
-        voices_.push_back(&v);
-    }
-
-    void remove_voice(voice& v) {
-        at_next_tick_.assert_in_queue_thread();
-        auto it = std::find(voices_.begin(), voices_.end(), &v);
-        assert(it != voices_.end());
-        voices_.erase(it);
-    }
-
-    void ticks_per_second(int tps) {
-        at_next_tick_.assert_in_queue_thread();
-        ticks_per_second_ = tps;
-    }
-
-    void global_volume(float vol) {
-        at_next_tick_.assert_in_queue_thread();
-        global_volume_ = vol;
-    }
-
-private:
-    static constexpr int sample_rate_ = 44100;
-
-    std::vector<voice*>  voices_;
-    std::vector<float>   mix_buffer_;
-    int                  next_tick_ = 0;
-    int                  ticks_per_second_ = 50; // 125 BPM = 125 * 2 / 5 = 50 Hz
-    float                global_volume_ = 1.0f;
-    job_queue            at_next_tick_;
-    // must be last
-    wavedev              wavedev_;
-
-    void tick() {
-        at_next_tick_.perform_all();
-    }
-
-    void render(short* s, int num_stereo_samples) {
-        mix_buffer_.resize(num_stereo_samples * 2);
-        float* buffer = &mix_buffer_[0];
-        memset(buffer, 0, num_stereo_samples * 2 * sizeof(float));
-        while (num_stereo_samples) {
-            if (!next_tick_) {
-                tick();
-                next_tick_ = sample_rate_ / ticks_per_second_;
-            }
-
-            const auto now = std::min(next_tick_, num_stereo_samples);
-
-            for (auto v : voices_) {
-                v->mix(buffer, now);
-            }
-
-            buffer             += now * 2;
-            num_stereo_samples -= now;
-            next_tick_         -= now;
-        }
-
-        for (size_t i = 0; i < mix_buffer_.size(); ++i) {
-            s[i] = sample_to_s16(mix_buffer_[i]*global_volume_);
-        }
-    }
-};
 
 class mod_player {
 public:
---
+++ mixer.h
@@ -0,0 +1,32 @@
+#ifndef SAMPEDIT_MIXER_H
+#define SAMPEDIT_MIXER_H
+
+#include <memory>
+
+#include <base/job_queue.h>
+#include <base/voice.h>
+
+class mixer {
+public:
+    explicit mixer();
+    ~mixer();
+
+    int sample_rate() const {
+        return sample_rate_;
+    }
+
+    void at_next_tick(const job_queue::job_type& job);
+
+    void add_voice(voice& v);
+    void remove_voice(voice& v);
+    void ticks_per_second(int tps);
+    void global_volume(float vol);
+
+private:
+    static constexpr int sample_rate_ = 44100;
+
+    class impl;
+    std::unique_ptr<impl> impl_;
+};
+
+#endif
